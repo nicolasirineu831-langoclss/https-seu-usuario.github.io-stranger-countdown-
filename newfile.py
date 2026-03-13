51cf41397a2f2bef5665d6453079bf18a4ddebae
nicolasirineu831-langoclss <nicolasirineu831@gmail.com>
2026-03-12 21:14:36 -0300

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,9 @@
+# range(início, fim, passo)
+# Começa em 2, vai até 20 (então o fim é 21), e incrementa de 2 em 2
+for i in range(2, 21, 2):
+    print(i)
+
+# OU, a forma mais parecida com a lógica do exercício 2 (com IF):
+# for i in range(2, 21):
+#     if i % 2 == 0:
+#         print(i)
